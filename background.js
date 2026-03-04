87e1f8a2ffac641c7a24c8770bf011df7037dbbe
feraxhp <feraxhp+gh@gmail.com>
2026-03-03 19:09:19 -0500

    fix: not preverving the state

--- background.js
+++ background.js
@@ -1,31 +1,38 @@
-// moz-extension://<uuid>/app/<urlencoded_url>
-const urlPrefix = browser.runtime.getURL("/app/");
-
-// Listener for navigation changes. Opens in popup if URL matches the prefix.
-browser.webNavigation.onCommitted.addListener(details => {
-  if (details.url.startsWith(urlPrefix)) {
-    const decodedURL = decodeURIComponent(details.url.substring(urlPrefix.length));
-    try {
-      browser.tabs.remove(details.tabId);
-      browser.windows.create({ type: 'popup', url: decodedURL });
-    } catch (error) {
-      console.error("Error opening popup window:", error); // Error handling
-    }
-  }
-}, { url: [{ urlPrefix }] });
-
-
-// Create context menu item.
 browser.contextMenus.create({
-  id: "pop-out-tab",
-  title: "Open as web app",
-  contexts: ["page", "link"] // Limited to more relevant contexts
+  id: "clone-pop-out-tab",
+  title: "Duplicate",
+  contexts: ["page", "link"],
+});
+
+browser.contextMenus.create({
+  id: "move-pop-out-tab",
+  title: "Preserve",
+  contexts: ["page", "link"],
 });
 
 
-// Handle context menu clicks. Opens the current URL in a popup window.
-browser.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === "pop-out-tab") {
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+  console.log(browser);
+  if (info.menuItemId === "clone-pop-out-tab") {
     browser.windows.create({ type: 'popup', url: tab.url });
   }
+  else
+  if (info.menuItemId === "move-pop-out-tab") {
+    await moveTabAndPreserveEssential(tab);
+  }
 });
+
+async function moveTabAndPreserveEssential(originalTab) {
+  await browser.windows.create({ tabId: originalTab.id, type: 'popup' });
+  if (!originalTab.pinned) return;
+  browser.tabs.create({
+    windowId: originalTab.windowId,
+    index: originalTab.index + 1,
+    url: originalTab.url,
+    pinned: originalTab.pinned,
+    active: false
+  }).then((tab) => {
+    tab.favicon = originalTab.favicon
+    browser.tabs.discard(tab.id)
+  })
+}
