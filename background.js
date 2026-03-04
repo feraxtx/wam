6862791452c87daf43ddb8720704e602f7067744
feraxhp <feraxhp+gh@gmail.com>
2026-03-03 19:34:02 -0500

    add: keyboard shortcuts

--- background.js
+++ background.js
@@ -1,27 +1,3 @@
-browser.contextMenus.create({
-  id: "clone-pop-out-tab",
-  title: "Duplicate",
-  contexts: ["page", "link"],
-});
-
-browser.contextMenus.create({
-  id: "move-pop-out-tab",
-  title: "Preserve",
-  contexts: ["page", "link"],
-});
-
-
-browser.contextMenus.onClicked.addListener(async (info, tab) => {
-  console.log(browser);
-  if (info.menuItemId === "clone-pop-out-tab") {
-    browser.windows.create({ type: 'popup', url: tab.url });
-  }
-  else
-  if (info.menuItemId === "move-pop-out-tab") {
-    await moveTabAndPreserveEssential(tab);
-  }
-});
-
 async function moveTabAndPreserveEssential(originalTab) {
   await browser.windows.create({ tabId: originalTab.id, type: 'popup' });
   if (!originalTab.pinned) return;
@@ -36,3 +12,37 @@ async function moveTabAndPreserveEssential(originalTab) {
     browser.tabs.discard(tab.id)
   })
 }
+
+browser.contextMenus.create({
+  id: "clone-pop-out-tab",
+  title: "as Copy",
+  contexts: ["page", "link"],
+});
+
+browser.contextMenus.create({
+  id: "move-pop-out-tab",
+  title: "Preserve",
+  contexts: ["page", "link"],
+});
+
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+  // console.log(browser);
+  if (info.menuItemId === "clone-pop-out-tab") {
+    browser.windows.create({ type: 'popup', url: tab.url });
+  }
+  else
+  if (info.menuItemId === "move-pop-out-tab") {
+    await moveTabAndPreserveEssential(tab);
+  }
+});
+
+browser.commands.onCommand.addListener(async (command) => {
+  const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+  if (command === "as_copy") {
+    browser.windows.create({ type: 'popup', url: tabs[0].url });
+  }
+  else
+  if (command === "preserve") {
+    await moveTabAndPreserveEssential(tabs[0]);
+  }
+});
